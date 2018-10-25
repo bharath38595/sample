5d3e6d54b5ec6cb4eeafd6a40a5795c963547e20
bharath38595 <rambharath38595@gmail.com>
2018-10-25 13:20:04 +0530

    created the base class

---
+++ src/com/git/GitExample.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hai this is my first git pgm");
+		
+	}
+
+}
